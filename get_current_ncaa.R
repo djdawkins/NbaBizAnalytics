d248cbda811054573423943627548a8f302694b5
ysojdehei <36141789+ysojdehei@users.noreply.github.com>
2022-07-15 15:36:53 -0500

    Add files via upload

--- get_current_ncaa.R
+++ get_current_ncaa.R
@@ -1,4 +1,4 @@
-
+### GET COLLEGE PLAYER STATS ###
 yrmin1 <- format(seq.Date(Sys.Date(), length=2, by='-1 year')[2], '%Y')
 yr <- format(Sys.Date(), '%Y')
 yr <- substr(yr,3,4)
@@ -46,17 +46,6 @@ for(i in 1:nrow(teamids)){
 }
 
 
-library(jsonlite)
-# 
-# # Convert lineups hash to list
-lineups_list <- list(values(lineups_hash))
-# # Convert list to JSON
-lineups_json = toJSON(lineups_list, pretty = TRUE, auto_unbox = TRUE)
-
-#write(lineups_json, "all_lineups.json")
-
-college_data <- fromJSON(lineups_json)
-
 
 
 
@@ -83,5 +72,94 @@ final_tbl %>%
   group_by(namePlayer) %>% 
   filter(n()>1)
 final_tbl <- final_tbl[!duplicated(final_tbl$namePlayer), ]
+
+#final_tbl <- final_tbl %>% mutate(namePlayerCol = namePlayer)
+final_tbl <- final_tbl %>% mutate(namePlayer = str_replace_all(namePlayer, fixed("'"), ""))
+#final_tbl <- final_tbl %>% mutate(namePlayerCol = str_replace_all(namePlayerCol, fixed("-"), ""))
+#final_tbl <- final_tbl %>% mutate(namePlayerCol = str_replace_all(namePlayerCol, fixed("."), ""))
+final_tbl <- final_tbl %>% mutate(namePlayer = str_replace_all(namePlayer, fixed("."), " "))
+final_tbl <- final_tbl %>% mutate(namePlayer = toupper(namePlayer))
+# Create namePlayerCol_2 column, then remove suffixes from names
+final_tbl <- final_tbl %>% mutate(namePlayer = str_replace_all(namePlayer, fixed("SR"), ""))
+final_tbl <- final_tbl %>% mutate(namePlayer = str_replace_all(namePlayer, fixed("JR"), ""))
+final_tbl <- final_tbl %>% mutate(namePlayer = str_replace_all(namePlayer, fixed("III"), ""))
+final_tbl <- final_tbl %>% mutate(namePlayer = str_replace_all(namePlayer, fixed("II"), ""))
+final_tbl <- final_tbl %>% mutate(namePlayer = str_replace_all(namePlayer, fixed("IV"), ""))
+
+
+
+
+
+
+### Fetch Positions ###
+
+library(hash)
+teamids <- teamids %>% filter(Season == tmp)
+# Create year_team
+positions_hash <- hash()
+# Loop through yr team tibble
+for(i in 1:nrow(teamids)){
+  #   
+  #   # Get year value
+  year <- tmp
+  
+  #   # GEt team value
+  team <- teamids[i,"Team"]
+  team <- team %>% getElement("Team")
+  #   
+  #   # Create year_team value
+  year_team <- paste0(year, team)
+  print(year_team)
+  #
+  
+  lineups <- get_team_roster(season = year, team.name = team)
+  #   
+  #   # Set hash key and save lineups tibble
+  positions_hash[[year_team]] <- lineups
+  # 
+}
+
+
+positions_list <- list(keys(positions_hash))
+datalist = list()
+for(i in 1:length(unlist(positions_list))){
+  
+  positions_college <- positions_list[[1]][i]
+  positions_tbl <- positions_hash[[positions_college]]
+  
+  
+  datalist[[i]] <- positions_tbl # add it to your list
+  
+}
+
+
+positions_tbl = do.call(rbind, datalist)
+
+positions_tbl <- positions_tbl %>% rename(namePlayer = Player)
+
+positions_tbl %>% 
+  group_by(namePlayer) %>% 
+  filter(n()>1)
+positions_tbl <- positions_tbl[!duplicated(positions_tbl$namePlayer), ]
+
+
+
+
+
+positions_tbl <- positions_tbl %>% mutate(namePlayer = str_replace_all(namePlayer, fixed("'"), ""))
+positions_tbl <- positions_tbl %>% mutate(namePlayer = str_replace_all(namePlayer, fixed("."), " "))
+positions_tbl <- positions_tbl %>% mutate(namePlayer = toupper(namePlayer))
+
+
+### Calculate AV and Join with position table ###
+
+
+final_tbl$TRB <- final_tbl$ORB + final_tbl$DRB
+final_tbl$credit <- (final_tbl$PTS + final_tbl$TRB + final_tbl$AST + final_tbl$STL + final_tbl$BLK) - (final_tbl$FGA - final_tbl$FGM) - (final_tbl$FTA - final_tbl$FTM) - final_tbl$TOV
+# AV
+final_tbl$Average_AV <- (final_tbl$credit**(0.75))/21
+final_tbl$Average_AV[is.na(final_tbl$Average_AV)] <- 0
+
+final_tbl <- inner_join(final_tbl, positions_tbl, by = "namePlayer")
 
 write_excel_csv(final_tbl, "current_collegePlayers.csv")
